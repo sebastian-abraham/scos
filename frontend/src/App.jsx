af7565395d26f72dd81fcb442c3326c433e38a6b
Sebastian Abraham <sebastianabraham2006@gmail.com>
2025-10-26 04:30:06 +0530

    frontend upgrade

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,22 +2,34 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import LoginPage from "./pages/LoginPage";
 import ProtectedRoute from "./config/ProtectedRoute";
+import { AuthProvider } from "./context/AuthContext";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <ProtectedRoute>
-              <div>Protected Home (replace with real content)</div>
-            </ProtectedRoute>
-          }
-        />
-        <Route path="/login" element={<LoginPage />} />
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
 
