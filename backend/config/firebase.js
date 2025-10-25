d679e65deffc6923483ab2b28a3ab555e1aaa2a7
Sebastian Abraham <sebastianabraham2006@gmail.com>
2025-10-26 04:43:29 +0530

    minor ifx

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -1,5 +1,13 @@
-var admin = require("firebase-admin");
-var serviceAccount = require("./serviceAccountKey.json");
+// Example: firebase.js
+const admin = require('firebase-admin');
+
+// Check if we are running on Render, if so, use the secret file path
+// Otherwise, use the local path
+const serviceAccountPath = process.env.RENDER 
+  ? '/etc/secrets/serviceAccountKey.json' 
+  : './serviceAccountKey.json';
+
+const serviceAccount = require(serviceAccountPath); 
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
