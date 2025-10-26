12326d682625ec594d6489a6fe3042562f589a7f
Sebastian Abraham <sebastianabraham2006@gmail.com>
2025-10-26 13:14:47 +0530

    Rotuing formanager and student

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,29 +4,41 @@ import LoginPage from "./pages/LoginPage";
 import ProtectedRoute from "./config/ProtectedRoute";
 import { AuthProvider } from "./context/AuthContext";
 import Dashboard from "./pages/Dashboard";
+import NotFoundPage from "./pages/NotFoundPage";
+// import ManagerDashboard from "./pages/ManagerDashboard";
+// import StudentDashboard from "./pages/StudentDashboard";
 
 function App() {
   return (
     <AuthProvider>
       <Router>
         <Routes>
+          {/* Public routes */}
+          <Route path="/login" element={<LoginPage />} />
+
+          {/* Student-only routes */}
           <Route
             path="/"
             element={
-              <ProtectedRoute>
+              <ProtectedRoute requiredRole="student">
                 <Dashboard />
+                {/* Or <StudentDashboard /> if you have a separate component */}
               </ProtectedRoute>
             }
           />
+
+          {/* Manager-only routes */}
           <Route
-            path="/dashboard"
+            path="/manager"
             element={
-              <ProtectedRoute>
+              <ProtectedRoute requiredRole="manager">
                 <Dashboard />
+                {/* Or <ManagerDashboard /> if you have a separate component */}
               </ProtectedRoute>
             }
           />
-          <Route path="/login" element={<LoginPage />} />
+          {/* 404 Not Found route */}
+          <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </Router>
     </AuthProvider>
